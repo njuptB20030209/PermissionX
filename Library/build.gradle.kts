c9673b977caf1c4c221872cd960308a2304641a8
buyunlong <buyunlong12138@gmail.com>
2024-04-16 16:48:38 +0800

    完成PermissionX库的开发与测试工作

--- Library/build.gradle.kts
+++ Library/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
 android {
-    namespace = "com.permissionx.app"
+    namespace = "com.permissionx.buyunlongdev"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.permissionx.app"
         minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,12 +37,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(project(":Library"))
-
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+
     }
 }
 dependencyResolutionManagement {
@@ -21,4 +22,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "PermissionX"
 include(":app")
- 
+include(":Library")
